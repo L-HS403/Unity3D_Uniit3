909add2372ed31e159d00aadfb98acb774e70bba
L-HS403 <leehs55315467@gmail.com>
2024-06-22 17:16:24 +0900

    3.2강 배경
    배경 반복 기능, 게임 오버 기능 추가

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    private PlayerController playerControllerScript;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2.0f;
     private float repeatRate = 2.0f;
@@ -12,10 +13,14 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
